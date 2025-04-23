77a43a2cb6bd8343cf2f450cf9ae20cbcce191c5
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-04-23 18:29:31 +0100

    Added 2 payment services that impement the paymentService interface

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -1,9 +1,13 @@
 package com.example.store;
 
 public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
-        var paymentService = new StripePaymentService();
         paymentService.processPayment(10);
     }
 }
