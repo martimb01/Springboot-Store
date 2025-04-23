34090ea477831617f33ef08500399b2e7de3eb5f
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-04-23 18:18:45 +0100

    Created PaymentService Interface

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -3,6 +3,7 @@ package com.example.store;
 public class OrderService {
 
     public void placeOrder() {
-        
+        var paymentService = new StripePaymentService();
+        paymentService.processPayment(10);
     }
 }
--- src/main/java/com/example/store/StripePaymentService.java
+++ src/main/java/com/example/store/StripePaymentService.java
@@ -1,6 +1,7 @@
 package com.example.store;
 
-public class StripePaymentService {
+public class StripePaymentService implements PaymentService {
+    @Override
     public void processPayment(double amount) {
         System.out.println("Stripe");
         System.out.println("Amount: " + amount + " paid.");
